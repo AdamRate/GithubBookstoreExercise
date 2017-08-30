9121da9eaef57087f99305a453077f00e9e183b6
AdamRate <Adamjrate@hotmail.co.uk>
2017-08-30 10:58:26 +0000

    ignoreFix

--- BookstoreProject/src/main/MainClass.java
+++ BookstoreProject/src/main/MainClass.java
@@ -2,4 +2,14 @@ package main;
 
 public class MainClass {
 
+	public static void main(String[] args) {
+	ServiceClass Sc = new ServiceClass();
+	
+	Sc.init();
+	Sc.autoBook();
+	Sc.addNewBook("Adam", "Book Title", "history", "4001");
+	Sc.showAllBooks();
+
+	}
+
 }
--- BookstoreProject/src/main/ServiceClass.java
+++ BookstoreProject/src/main/ServiceClass.java
@@ -1,5 +1,42 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+import main.Book;
+
 public class ServiceClass {
+
+	private int ID;
+	private Map<Integer, Book> bookMap;
+
+	public void init() {
+		this.bookMap = new HashMap<Integer, Book>();
+		ID =1;
+	}
+
+	public void addNewBook(String author, String title, String genre, String yearPublished ) {
+		ID++;
+		Book newBook = new Book(author, title, genre, yearPublished);
+		bookMap.put(ID, newBook);
+	}
+	
+	public void autoBook(){
+		Book aBook = new Book("Test Author", "Test Title", "Test Genre", "Test Year");
+		bookMap.put(1, aBook);
+	}
+	
+	public void showAllBooks(){
+		for (int num = 0; num<bookMap.size(); num++){
+			System.out.println(bookMap.get(num+1));
+		}
+	}
+	
+	public void removeBook(int i){
+		bookMap.remove(i);
+	}
+	
+	public void showABook(int i){
+		System.out.println(bookMap.get(i));
+	}
 
 }
